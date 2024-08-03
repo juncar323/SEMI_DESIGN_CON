0250ea83ddfb4ffd9270670f2d452cc7d5b17676
JUN___ <juncar323@gmail.com>
2024-08-03 20:29:20 +0900

    Update filter.v

--- filter.v
+++ filter.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/02/2024 02:17:05 PM
-// Design Name: 
-// Module Name: filter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module filter ( 
